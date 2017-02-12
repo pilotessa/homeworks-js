2ab2b18da9290ba887e70074e3ef15452bc1e7df
pilotessa <pilotessa@gmail.com>
2017-02-12 20:05:02 +0200

    Generate the random table

--- js/tasks.js
+++ js/tasks.js
@@ -33,7 +33,7 @@ function task2() {
         if (!isNaN(number)) {
             result += number;
         }
-    } while (Boolean(number));
+    } while (number);
 
     alert(result);
 }
@@ -154,6 +154,51 @@ function task6() {
     for (var key in book2) {
         console.log(key + ": " + book2[key]);
     }
+}
+
+/*
+ * Напишите функцию для создания таблицы размером NхM. Такой, чтобы каждая ячейка заполнялась случайной буквой русского алфавита и случайным цветом фона
+ */
+function task7() {
+    var m = +prompt("Введите количество строк");
+    var n = +prompt("Введите количество столбцов");
+
+    if (Boolean(m) && m % 1 === 0 && Boolean(n) && n % 1 === 0) {
+        console.log(randTable(m, n));
+    }
+}
+
+function randTable(m, n) {
+    var result = '<table>';
+
+    for (var i = 0; i < m; i++) {
+        result += '<tr>';
+        for (var j = 0; j < n; j++) {
+            result += randCell();
+        }
+        result += '</tr>';
+    }
+    result += '</table>';
+
+    return result;
+}
+
+function randCell() {
+    return '<td style="background-color: ' + randColor() + '">' + randLetter() + '</td>';
+}
+
+function randColor() {
+    var CSS_COLOR_NAMES = ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "Darkorange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"];
+    var position = Math.floor(Math.random() * (CSS_COLOR_NAMES.length));
+
+    return CSS_COLOR_NAMES[position];
+}
+
+function randLetter() {
+    var CYRILLIC_SYMBOLS = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя';
+    var position = Math.floor(Math.random() * (CYRILLIC_SYMBOLS.length));
+
+    return CYRILLIC_SYMBOLS.charAt(position);
 }
 
 // Bind functionality
