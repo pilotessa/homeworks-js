e1815ce5144940fe88ae8f88e427c00ce6012b0c
pilotessa <pilotessa@gmail.com>
2017-06-27 22:49:27 +0300

    Implement sorting and output switching

--- angular/assets/js/main.js
+++ angular/assets/js/main.js
@@ -40650,6 +40650,30 @@ app.controller('PageController', function () {
 
     this.order = this.ORDER_PRICE_ASC;
     this.output = this.OUTPUT_TABLE;
+
+    this.getOrderBy = function () {
+        switch (this.order) {
+            case this.ORDER_PRICE_ASC:
+                return 'price';
+            case this.ORDER_PRICE_DESC:
+                return '-price';
+            case this.ORDER_NAME_ASC:
+                return 'name';
+            case this.ORDER_NAME_DESC:
+                return '-name';
+            default:
+                return 'price';
+        }
+    };
+
+    this.getOutputClass = function () {
+        switch (this.output) {
+            case this.OUTPUT_TABLE:
+                return 'row-table';
+            case this.OUTPUT_LIST:
+                return 'row-list';
+        }
+    }
 });
 var random = {
     name: function () {
